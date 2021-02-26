ada8c12b60c0bf2679bf9b1ca8bf2acea0cd85b2
QuentinLeFourbe <quentingarcia40@gmail.com>
2021-02-26 11:25:16 +0100

    update env

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import busboy from 'busboy'
 import postRoutes from './routes/posts.js';
 import mealRoutes from './routes/meals.js';
+import multer from 'multer'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app = express();
 
@@ -14,7 +17,8 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/meals', mealRoutes);
 
-const URL = 'mongodb+srv://admin:Cacadu40@cluster0.jmrhk.mongodb.net/MyMenu?retryWrites=true&w=majority';
+const URL = process.env.ATLAS_URI;
+
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(URL, {
---
+++ server/middleware/upload.js
@@ -0,0 +1,3 @@
+import util from 'util';
+import multer from 'multer'
+import GridFsStorage from 'multer-gridfs-storage'
