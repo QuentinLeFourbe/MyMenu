fa9b14b9de5191861e1f1ff9efbb2b5159b063e9
QuentinLeFourbe <quentingarcia40@gmail.com>
2021-05-31 16:15:53 +0200

    client proxy linked to server url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'http://localhost:5000',
+            target: 'https://mes-menus.herokuapp.com/',
+            // target: 'http://localhost:5000',
             changeOrigin: true,
         })
     );
